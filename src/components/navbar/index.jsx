e452b31c1d4efa74262412f5af43b42bf6894051
moussasg <117439847+moussasg@users.noreply.github.com>
2023-09-13 13:08:13 +0100

    finpageone

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,26 +2,47 @@ import React from 'react'
 import Style from "./index.module.css"
 export default function Navbrar() {
   return (
+    <>
 <div className={Style.container}>
+<div className={Style.logo}>
+       <h3 className={Style.funiro}>Funiro.</h3>
+       <div className={Style.coloradress}>
+        400 University Drive Suite 200 Coral Gables
+        <div>FL 33134 USA</div>
+        </div>
+</div>
   <div className={Style.row}>
     <div className={Style.column}> {/*first column*/}
-      <p>Mot 1</p>
-      <p>Mot 2</p>
-      <p>Mot 3</p>
-      <p>mot 4</p>
-      <p>mot 5</p>
+      <p className={Style.pstyle}>Links</p>
+      <p>Home</p>
+      <p>Shop</p>
+      <p>About</p>
+      <p>Contact</p>
+    </div>
+    <div className={Style.column}> {/*second column*/}
+    <div className={Style.helpandpaymen}>
+      <p className={Style.pstyle}>Help</p>
+      <p>Payment Options</p>
+      <p>Returns</p>
+      <p>Privacy Policies</p>
+      </div>
     </div>
     <div className={Style.column}>
-      <p>Mot 4</p>
-      <p>Mot 5</p>
-      <p>Mot 6</p>
-    </div>
-    <div className={Style.column}>
-      <p>Mot 7</p>
-      <p>Mot 8</p>
-      <p>Mot 9</p>
+      <div className={Style.newslet}>
+        <div className={Style.newplussubsc}>
+        <p className={Style.pstyle}>Newsletter</p>
+        </div>
+      <div className={Style.subscribe}>
+       <input type='text' value="Enter Your Email Adress"></input> 
+      <p>SUBSCRIBE</p>
+      </div>
+        </div>
     </div>
   </div>
 </div>
+<div className={Style.allright}>
+    <p>2023 furino. All rights reverved</p>
+    </div>
+</>
   )
 }
