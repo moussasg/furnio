dfbf22b293d0234948904fdbe81783aae6a25a36
moussasg <117439847+moussasg@users.noreply.github.com>
2023-09-16 11:57:08 +0100

    push

--- src/components/componenetLogin/rightcomponent/index.jsx
+++ src/components/componenetLogin/rightcomponent/index.jsx
@@ -48,9 +48,10 @@ export default function Rightcomponenet() {
         <div className={Style.title}>
           <div className={Style.createtitle}>
           <p>{message}</p>
-          <div className={Style.createandgetstart}></div>
+          <div className={Style.createandgetstart}>
             <div className={Style.createac}>Create an account</div>
             <div className={Style.letsget}>Let’s get started with your 30 days free trial</div>
+            </div>
           </div>
         </div>
         <div className={Style.register}>
