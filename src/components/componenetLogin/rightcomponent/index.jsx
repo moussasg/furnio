fda1f9bfa62e7c8c57029fe0ac9628734d8ec6d9
moussasg <117439847+moussasg@users.noreply.github.com>
2023-09-18 11:53:35 +0100

    push

--- src/components/componenetLogin/rightcomponent/index.jsx
+++ src/components/componenetLogin/rightcomponent/index.jsx
@@ -25,7 +25,7 @@ export default function Rightcomponenet() {
     setspin(<Circular/>)
     event.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3002/Home', { email, password, urname });
+      const response = await axios.post('https://dev.doxycode.com/Home', { email, password, urname });
       console.log(response);
       if (response.data.success === true) {
         const token = response.data.token;
